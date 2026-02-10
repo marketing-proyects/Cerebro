8f85bfb49c1f0314ab2be54f1b54814d6c7fa952
Wurth <fortezagerman@gmail.com>
2026-02-10 17:51:31 -0300

    Update auth_manager.py

--- modules/auth_manager.py
+++ modules/auth_manager.py
@@ -1,30 +1,23 @@
 import streamlit as st
-import requests
-from io import BytesIO
-
-def obtener_logo_seguro():
-    """Descarga el logo y lo devuelve como bytes para evitar errores de renderizado"""
-    url = "https://upload.wikimedia.org/wikipedia/commons/b/be/W%C3%BCrth_logo.svg"
-    try:
-        response = requests.get(url)
-        return response.content
-    except:
-        return None
 
 def gestionar_login():
     if "autenticado" not in st.session_state:
         st.session_state["autenticado"] = False
 
     if not st.session_state["autenticado"]:
-        # CSS para centrar el logo y ocultar el '0' fantasma
+        # CSS para centrar y limpiar
         st.markdown("""
             <style>
             .stApp { background-color: #FFFFFF !important; }
-            .login-header {
+            .login-container {
                 display: flex;
                 flex-direction: column;
                 align-items: center;
-                margin-top: 30px;
+                justify-content: center;
+                padding-top: 50px;
+            }
+            .logo-img {
+                width: 200px;
                 margin-bottom: 20px;
             }
             div.stButton > button {
@@ -37,20 +30,18 @@ def gestionar_login():
             </style>
         """, unsafe_allow_html=True)
 
-        # Contenedor del Logo
-        logo_data = obtener_logo_seguro()
-        st.markdown('<div class="login-header">', unsafe_allow_html=True)
-        if logo_data:
-            st.image(logo_data, width=180)
-        else:
-            st.markdown("<h1 style='color: #ED1C24;'>WÜRTH</h1>", unsafe_allow_html=True)
-        st.markdown("<h3 style='color: #333333; margin-top: 10px;'>ACCESO CEREBRO</h3>", unsafe_allow_html=True)
-        st.markdown('</div>', unsafe_allow_html=True)
+        # Insertar logo mediante HTML puro (evita errores de PIL)
+        st.markdown("""
+            <div class="login-container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/b/be/W%C3%BCrth_logo.svg" class="logo-img">
+                <h2 style='color: #ED1C24; font-family: sans-serif;'>ACCESO CEREBRO</h2>
+            </div>
+        """, unsafe_allow_html=True)
         
         with st.container():
             col1, col2, col3 = st.columns([1, 2, 1])
             with col2:
-                with st.form(key="login_central_final"):
+                with st.form(key="login_form_v10"):
                     user = st.text_input("Usuario")
                     password = st.text_input("Contraseña", type="password")
                     if st.form_submit_button("INGRESAR AL SISTEMA"):
