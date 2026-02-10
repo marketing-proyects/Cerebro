ae7ab7b70b3f08c11c080fe04f05178d61b0f698
Wurth <fortezagerman@gmail.com>
2026-02-10 17:47:19 -0300

    Update auth_manager.py

--- modules/auth_manager.py
+++ modules/auth_manager.py
@@ -5,14 +5,15 @@ def gestionar_login():
         st.session_state["autenticado"] = False
 
     if not st.session_state["autenticado"]:
+        # Estilos para centrar el logo y arreglar el botón
         st.markdown("""
             <style>
             .stApp { background-color: #FFFFFF !important; }
-            .login-container {
+            .login-box {
                 display: flex;
                 flex-direction: column;
                 align-items: center;
-                margin-top: 50px;
+                margin-top: 20px;
                 margin-bottom: 20px;
             }
             div.stButton > button {
@@ -25,16 +26,16 @@ def gestionar_login():
             </style>
         """, unsafe_allow_html=True)
 
-        # Logo centrado arriba del cuadro de login
-        st.markdown('<div class="login-container">', unsafe_allow_html=True)
+        # Logo centrado arriba del formulario
+        st.markdown('<div class="login-box">', unsafe_allow_html=True)
         st.image("https://upload.wikimedia.org/wikipedia/commons/b/be/W%C3%BCrth_logo.svg", width=180)
+        st.markdown("<h2 style='color: #ED1C24; margin-top: 15px;'>ACCESO AL SISTEMA</h2>", unsafe_allow_html=True)
         st.markdown('</div>', unsafe_allow_html=True)
         
         with st.container():
             col1, col2, col3 = st.columns([1, 2, 1])
             with col2:
-                with st.form(key="login_central_v2"):
-                    st.markdown("<h3 style='text-align: center; color: #ED1C24;'>ACCESO AL SISTEMA</h3>", unsafe_allow_html=True)
+                with st.form(key="login_central_rigid"):
                     user = st.text_input("Usuario")
                     password = st.text_input("Contraseña", type="password")
                     if st.form_submit_button("INGRESAR"):
